d2118787a6b5fe02963bbe3a4b23747593c402d3
Malcomn <malcomn.j.mitchell@gmail.com>
2023-01-17 14:13:29 -0500

    init
    init

---
+++ RickAndMorthyApp/Utilities/StringToColorConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace RickAndMorthyApp.Utilities
+{
+    public class StringToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // WRITE CONVERTER LOGIC HERE
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
